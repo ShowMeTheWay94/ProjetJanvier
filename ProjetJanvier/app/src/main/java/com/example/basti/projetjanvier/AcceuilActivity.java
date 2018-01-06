1b00519741f6f72c2cebfbdb828fe3f7f8209de5
bastien <bastiendewez@hotmail.com>
2018-01-06 20:15:51 +0100

    Page d'accueil et d'ajout d'article

---
+++ ProjetJanvier/app/src/main/java/com/example/basti/projetjanvier/AcceuilActivity.java
@@ -0,0 +1,48 @@
+package com.example.basti.projetjanvier;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AcceuilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acceuil);
+
+        Button ajoutArticle = (Button)findViewById(R.id.ajoutArticle);
+        Button recherchePrix = (Button)findViewById(R.id.recherchePrix);
+        Button rechercheVille = (Button)findViewById(R.id.rechercheVille);
+
+        ajoutArticle.setOnClickListener(ajout);
+        recherchePrix.setOnClickListener(prix);
+        rechercheVille.setOnClickListener(ville);
+    }
+
+    private View.OnClickListener ajout = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(AcceuilActivity.this, AjoutArticle.class);
+            startActivity(intent);
+        }
+    };
+
+    private View.OnClickListener prix = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(AcceuilActivity.this, AjoutArticle.class);
+            startActivity(intent);
+        }
+    };
+
+    private View.OnClickListener ville = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(AcceuilActivity.this, AjoutArticle.class);
+            startActivity(intent);
+        }
+    };
+}
---
+++ ProjetJanvier/app/src/main/java/com/example/basti/projetjanvier/AjoutArticle.java
@@ -0,0 +1,12 @@
+package com.example.basti.projetjanvier;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AjoutArticle extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ajoutarticle);
+    }
+}
